9984b9a93477f97ccb376eadfd145dfbe2a32883
funkill2 <funkill2@gmail.com>
2020-12-19 14:40:43 +0400

    changed getting pr number

--- crates/common/src/github.rs
+++ crates/common/src/github.rs
@@ -1,4 +1,6 @@
-use hubcaps::{Credentials, Github as HCGithub, comments::CommentOptions, repositories::Repository};
+use hubcaps::{
+    comments::CommentOptions, repositories::Repository, Credentials, Github as HCGithub,
+};
 pub struct Github {
     repo: Repository,
 }
@@ -17,23 +19,31 @@ impl Github {
     }
 
     pub fn pr_number() -> crate::Result<u64> {
-        let github_ref = std::env::var("GITHUB_REF")?;
-        github_ref
-            .split('/')
-            .nth(2)
-            .expect(&format!(
-                "github_ref has unknown format: {}. Must be 'refs/pull/:prNumber/merge'",
-                github_ref
-            ))
-            .parse()
-            .map_err(Into::into)
+        use json::JsonValue;
+
+        let event_path = std::env::var("GITHUB_EVENT_PATH")?;
+        let event = std::fs::read_to_string(event_path)?;
+        let value = json::parse(&event)?;
+
+        match value {
+            JsonValue::Object(value) => value
+                .get("number")
+                .and_then(JsonValue::as_u64)
+                .ok_or_else(|| anyhow::format_err!("Can't get PR number from event")),
+            _ => Err(anyhow::format_err!("Event not object")),
+        }
     }
 
     pub async fn comment_pr(&self, text: String) -> crate::Result<()> {
         let pr_number = Github::pr_number()?;
         let comment = CommentOptions { body: text };
 
-        self.repo.pulls().get(pr_number).comments().create(&comment).await?;
+        self.repo
+            .pulls()
+            .get(pr_number)
+            .comments()
+            .create(&comment)
+            .await?;
 
         Ok(())
     }
