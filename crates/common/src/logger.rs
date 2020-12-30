57cd83cf9ea3b5aa31e57fd6aa0bf0006dbca36c
funkill2 <funkill2@gmail.com>
2020-12-30 13:32:21 +0400

    fixed logger

--- crates/common/src/logger.rs
+++ crates/common/src/logger.rs
@@ -1,3 +1,29 @@
-pub fn init_logger() {
-    pretty_env_logger::init_timed();
+use log::Level;
+use std::io::Write;
+
+pub struct Logger;
+
+impl Logger {
+    pub fn init() {
+        pretty_env_logger::formatted_builder()
+            .format(|buf, record| match record.level() {
+                Level::Debug | Level::Trace => writeln!(buf, "::debug::{}", record.args()),
+                Level::Info | Level::Warn => writeln!(
+                    buf,
+                    "::warning file={},line={}::{}",
+                    record.file().unwrap_or_default(),
+                    record.line().unwrap_or_default(),
+                    record.args()
+                ),
+                Level::Error => writeln!(
+                    buf,
+                    "::error file={},line={}::{}",
+                    record.file().unwrap_or_default(),
+                    record.line().unwrap_or_default(),
+                    record.args()
+                ),
+            })
+            .parse_filters(&std::env::var("RUST_LOG").unwrap_or_else(|_| "info".into()))
+            .init();
+    }
 }
--- crates/sym-counter/src/main.rs
+++ crates/sym-counter/src/main.rs
@@ -1,26 +1,35 @@
-use common::{git::Git, github::Github};
+use common::{git::Git, github::Github, logger::Logger};
 use std::path::Path;
 
 type Result<T> = anyhow::Result<T>;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    Logger::init();
+
+    let users = std::env::var("INPUT_TAGGED_USERS").unwrap_or_default();
+
+    let mut symbols = 0;
+    let mut files = 0;
+    let changed_files = Git.changed_files()?;
+    for filename in changed_files {
+        let symbols_count = count_for_file(&filename);
+        if filename.ends_with(".md") {
+            symbols += symbols_count;
+            files += 1;
+        }
+
+        log::info!("File `{}`, symbols: {}", filename, symbols_count);
+    }
+
+    log::info!("Filtered files: {}, symbols: {}", files, symbols);
+
     let gh_token = std::env::var("GITHUB_TOKEN")?;
-    let github = Github::new(gh_token)?;
-
-    let (files_count, sym_count) = Git
-        .changed_files()?
-        .iter()
-        .filter(|file| file.ends_with("md"))
-        .map(count_for_file)
-        .fold((0, 0), |(files, symbols), count| {
-            (files + 1, symbols + count)
-        });
-
-    println!("Files: {}, symbols: {}", files_count, sym_count);
-
-    github
-        .comment_pr(format!("Файлов: {}, символов: {}", files_count, sym_count))
+    Github::new(gh_token)?
+        .comment_pr(format!(
+            "Файлов: {}, символов: {}. {} fyi",
+            files, symbols, users
+        ))
         .await
 }
 
@@ -28,7 +37,7 @@ fn count_for_file<P: AsRef<Path>>(file: P) -> usize {
     match std::fs::read_to_string(file) {
         Ok(content) => content.chars().count(),
         Err(e) => {
-            log::info!("Read file to string error: {:?}", e);
+            log::warn!("Read file to string error: {:?}", e);
             0
         }
     }
